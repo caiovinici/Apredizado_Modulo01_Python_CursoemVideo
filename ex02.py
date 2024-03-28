220af88f41bb6c9e7d39004d23983fa5a634e175
Caio Vinicius Ferreira <129127743+caiovinici@users.noreply.github.com>
2024-03-27 22:03:02 -0300

    Add files via upload

---
+++ ex02.py
@@ -0,0 +1,11 @@
+# Respondendo ao usuário
+
+print('---- Desafio 02 ----')
+nome = input('Qual seu nome? ')
+print('Olá',nome,'Seja bem - vindo!')
+print('Por favor nós informe os dados abaixo.')
+Dia = input('Qual a data do seu nascimento?: ')
+Mes = input('Qual mes?: ')
+Ano = input('Qual ano?: ')
+Cidade = input('Qual cidade nasceu? ') 
+print(f'Você nasceu no dia{Dia} de {Mes} de {Ano} {Cidade},correto!?')
